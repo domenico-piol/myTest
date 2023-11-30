6ecae2f882c1a7c56741ed3f55f74cfb6f4d9b27
Domenico Piol <domenico@piol.name>
2023-11-30 08:21:28 +0100

    refactor package names

---
+++ src/main/java/name/piol/test/myTest/GreetingRestController.java
@@ -0,0 +1,20 @@
+package name.piol.test.myTest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+public class GreetingRestController {
+
+    Logger logger = LoggerFactory.getLogger(GreetingRestController.class);
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        logger.info("Greeting REST service requested...");
+        return "greetings...";
+    }
+
+}
+
--- src/main/java/name/piol/test/myTest/WelcomeController.java
+++ src/main/java/name/piol/test/myTest/WelcomeController.java
@@ -1,10 +1,8 @@
 package name.piol.test.myTest;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
