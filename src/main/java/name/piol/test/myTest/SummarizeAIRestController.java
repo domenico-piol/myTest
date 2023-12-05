28108350fe29912f6bb0a239e000cd4f0119c673
Domenico Piol <domenico@piol.name>
2023-12-05 13:29:43 +0100

    adding summarize AI class

---
+++ src/main/java/name/piol/test/myTest/SummarizeAIRestController.java
@@ -0,0 +1,22 @@
+package name.piol.test.myTest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+public class SummarizeAIRestController {
+
+    Logger logger = LoggerFactory.getLogger(SummarizeAIRestController.class);
+
+    @GetMapping(value = "/summarize/{article}")
+    public String greeting(@PathVariable String article) {
+        logger.info("Summarizing a text...");
+        logger.debug(article);
+        return "I am sorry Dave, you can't do that!";
+    }
+
+}
+
